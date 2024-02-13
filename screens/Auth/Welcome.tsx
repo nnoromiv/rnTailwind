1e4549e58ab27d70855ef642921d64638723cffc
nnoromiv <nnorom.prince44@gmail.com>
2024-02-13 13:33:44 +0100

    Update

--- screens/Auth/Welcome.tsx
+++ screens/Auth/Welcome.tsx
@@ -7,10 +7,10 @@ const Welcome = ({ navigation }: any) => {
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`px-3 gap-4 flex justify-center items-center m-auto`}>
-                <Text style={tw`font-bold text-black text-3xl mb-2`}>Welcome by NnoromIV</Text>
+                <Text style={tw`font-bold text-black text-3xl mb-2`}>Welcome by NNOROMIV</Text>
                 <Text style={tw`font-light text-black text-base`}>Begin editing page</Text>
                 {/* Example Usage of Custom Button */}
-                <Button title='Author' style={`px-6 py-2`} onPress={() => Linking.openURL('https://github.com/nnoromiv')} />
+                <Button title='Visit Author' style={`px-6 py-2`} onPress={() => Linking.openURL('https://github.com/nnoromiv')} />
             </View>
         </SafeAreaView>
     )
